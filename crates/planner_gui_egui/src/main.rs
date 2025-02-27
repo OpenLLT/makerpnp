4e340fb69a40a52fd3dc2213be9900cf54ba78a7
Dominic Clifton <me@dominicclifton.name>
2025-02-27 13:02:52 +0100

    PlannerGUI - Add i18n and tracing init code.

--- crates/planner_gui_egui/src/main.rs
+++ crates/planner_gui_egui/src/main.rs
@@ -1,5 +1,10 @@
 use egui_mobius::factory;
 use egui_mobius::types::Value;
+use tracing::info;
+use tracing_subscriber::{fmt, EnvFilter};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use i18n::I18nConfig;
 use crate::ui_app::{UiApp, UiState};
 use crate::ui_commands::{handle_command, UiCommand};
 
@@ -8,6 +13,24 @@ mod ui_app;
 mod ui_commands;
 
 fn main() {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+
+    info!("Started");
+
+    i18n::init(
+        I18nConfig {
+            languages: vec![
+                String::from("en-US"), 
+                String::from("es-ES")
+            ],
+            default: "en-US".to_string(),
+            fallback: "en-US".to_string(),
+        }
+    );
+
     let (signal, slot) = factory::create_signal_slot::<UiCommand>();
 
     let ui_state = Value::new(UiState::default());
