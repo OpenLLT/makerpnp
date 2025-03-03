12f3b0c2030cf0e7ef69775c4ac3da2b7cdceb3e
Dominic Clifton <me@dominicclifton.name>
2025-03-03 12:54:19 +0100

    PlannerGUI - Show names in tree view instead of paths.  Add Arg to FluentArgs conversion to i18n crate.

--- crates/i18n/src/lib.rs
+++ crates/i18n/src/lib.rs
@@ -32,53 +32,76 @@ pub fn init(config: I18nConfig) {
 }
 
 pub mod fluent_argument_helpers {
-    use fluent_bundle::types::{FluentNumber, FluentNumberOptions};
-    use fluent_bundle::{FluentArgs, FluentValue};
-    use serde_json::Value;
-    use std::borrow::Cow;
-    use std::collections::HashMap;
-    use tracing::trace;
+    
+    #[cfg(feature = "json")]
+    pub mod json {
+        use fluent_bundle::types::{FluentNumber, FluentNumberOptions};
+        use fluent_bundle::{FluentArgs, FluentValue};
+        use serde_json::Value;
+        use std::borrow::Cow;
+        use std::collections::HashMap;
+        use tracing::trace;
 
-    pub fn build_fluent_args<'a>(params: &'a HashMap<Cow<'_, str>, Value>) -> FluentArgs<'a> {
-        let mut args = egui_i18n::fluent::FluentArgs::new();
-        for (key, value) in params.iter() {
-            match value {
-                Value::Null => {
-                    trace!("encountered null value for field: {}", key);
+        pub fn build_fluent_args<'a>(params: &'a HashMap<Cow<'_, str>, Value>) -> FluentArgs<'a> {
+            let mut args = egui_i18n::fluent::FluentArgs::new();
+            for (key, value) in params.iter() {
+                match value {
+                    Value::Null => {
+                        trace!("encountered null value for field: {}", key);
+                    }
+                    Value::Bool(_) => todo!(),
+                    Value::Number(number) => {
+                        // TODO make sure this is correct!  perhaps write some integration tests to prove the conversion is correct.
+                        if number.is_f64() {
+                            let value = FluentValue::Number(FluentNumber::new(
+                                number.as_f64().unwrap(),
+                                FluentNumberOptions::default(),
+                            ));
+                            args.set(key.to_string(), value);
+                        } else if number.is_i64() {
+                            let value = FluentValue::Number(FluentNumber::new(
+                                number.as_i64().unwrap() as f64,
+                                FluentNumberOptions::default(),
+                            ));
+                            args.set(key.to_string(), value);
+                        } else if number.is_u64() {
+                            let value = FluentValue::Number(FluentNumber::new(
+                                number.as_u64().unwrap() as f64,
+                                FluentNumberOptions::default(),
+                            ));
+                            args.set(key.to_string(), value);
+                        } else {
+                            unreachable!()
+                        }
+                    }
+                    Value::String(string) => {
+                        let value = FluentValue::String(string.into());
+                        args.set(key.to_string(), value);
+                    }
+                    Value::Array(_) => todo!(),
+                    Value::Object(_) => todo!(),
                 }
-                Value::Bool(_) => todo!(),
-                Value::Number(number) => {
-                    // TODO make sure this is correct!  perhaps write some integration tests to prove the conversion is correct.
-                    if number.is_f64() {
-                        let value = FluentValue::Number(FluentNumber::new(
-                            number.as_f64().unwrap(),
-                            FluentNumberOptions::default(),
-                        ));
-                        args.set(key.to_string(), value);
-                    } else if number.is_i64() {
-                        let value = FluentValue::Number(FluentNumber::new(
-                            number.as_i64().unwrap() as f64,
-                            FluentNumberOptions::default(),
-                        ));
-                        args.set(key.to_string(), value);
-                    } else if number.is_u64() {
-                        let value = FluentValue::Number(FluentNumber::new(
-                            number.as_u64().unwrap() as f64,
-                            FluentNumberOptions::default(),
-                        ));
-                        args.set(key.to_string(), value);
-                    } else {
-                        unreachable!()
+            }
+            args
+        }
+    }
+
+    #[cfg(feature = "planner_app")]
+    pub mod planner_app {
+        use std::collections::HashMap;
+        use fluent_bundle::{FluentArgs, FluentValue};
+        use planner_app::Arg;
+
+        pub fn build_fluent_args(args: &HashMap<String, Arg>) -> FluentArgs {
+            let mut fluent_args = FluentArgs::new();
+            for (key, value) in args.iter() {
+                match value {
+                    Arg::String(value) => {
+                        fluent_args.set(key, FluentValue::String(value.into()));
                     }
                 }
-                Value::String(string) => {
-                    let value = FluentValue::String(string.into());
-                    args.set(key.to_string(), value);
-                }
-                Value::Array(_) => todo!(),
-                Value::Object(_) => todo!(),
             }
+            fluent_args
         }
-        args
     }
 }
