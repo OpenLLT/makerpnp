f1e64804fea51c76fd9caccb725980d52547478c
Dominic Clifton <me@dominicclifton.name>
2025-03-09 19:28:05 +0100

    PlannerGUI - Ensure the add pcb dialog can only be submitted when the form is valid.  Extract the form code to a module and struct.

---
+++ crates/planner_gui_egui/src/forms/mod.rs
@@ -0,0 +1,157 @@
+use std::sync::mpsc::Sender;
+use egui::{Response, RichText, Ui};
+use egui_i18n::translate_fluent;
+use egui_mobius::types::{Value, ValueGuard};
+use egui_taffy::taffy::{AlignItems, AlignSelf, Display, FlexDirection, Style};
+use egui_taffy::{Tui, TuiBuilderLogic, TuiContainerResponse};
+use egui_taffy::taffy::prelude::{fit_content, fr, length, percent, span};
+use validator::{Validate, ValidationErrors};
+use i18n::fluent_argument_helpers::json::build_fluent_args;
+
+/// transient helper
+pub struct Form<F, C> {
+    validation_errors: Result<(), ValidationErrors>,
+    fields: Value<F>,
+    sender: Sender<C>,
+}
+
+impl<F: Validate, C> Form<F, C> {
+    pub fn new(fields: &Value<F>, sender: &Sender<C>) -> Self {
+        let fields = fields.clone();
+        let sender = sender.clone();
+        let validation_errors = {
+            let fields = fields.lock().unwrap();
+
+            fields.validate()
+        };
+
+        Self {
+            validation_errors,
+            fields,
+            sender,
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.validation_errors.is_ok()
+    }
+
+    pub fn show_fields(&self, tui: &mut Tui, fields: impl FnOnce(&Self, &mut Tui)) {
+
+        let default_style = Self::form_default_style();
+
+        //
+        // form fields container
+        //
+        tui.style(Style {
+            flex_direction: FlexDirection::Row,
+            align_self: Some(AlignSelf::Stretch),
+            ..default_style()
+        })
+            .add(|tui| {
+                //
+                // grid container
+                //
+                tui.style(Style {
+                    flex_grow: 1.0,
+                    display: Display::Grid,
+                    grid_template_columns: vec![fit_content(percent(1.)), fr(1.)],
+                    grid_template_rows: vec![fr(1.), fr(1.)],
+
+                    // ensure items are centered vertically on rows
+                    align_items: Some(AlignItems::Center),
+                    ..default_style()
+                })
+                    .add(|tui| {
+                        fields(&self, tui);
+                        // end of grid container content
+                    });
+
+                // end of form fields container content
+            });
+    }
+
+    fn form_default_style() -> fn() -> Style {
+        let default_style = || Style {
+            padding: length(2.),
+            gap: length(2.),
+            ..Default::default()
+        };
+
+        default_style
+    }
+
+    pub fn add_field(
+        &self,
+        field_name: &str,
+        label: String,
+        tui: &mut Tui,
+        mut ui_builder: impl FnMut(&mut Ui, ValueGuard<'_, F>, Sender<C>) -> Response)
+    {
+        let default_style = Self::form_default_style();
+
+        tui.style(Style { ..default_style() }).add(|tui| {
+            tui.label(label);
+        });
+
+        tui.style(Style {
+            flex_grow: 1.0,
+            ..default_style()
+        })
+            .add(|tui| {
+                tui.style(Style {
+                    flex_grow: 1.0,
+                    ..default_style()
+                })
+                    .ui_add_manual(
+                        | ui | {
+                            ui_builder(ui, self.fields.lock().unwrap(), self.sender.clone())
+                        },
+                        Self::no_transform,
+                    );
+            });
+        
+        Self::field_error(&self.validation_errors, default_style, tui, field_name);
+    }
+
+    fn no_transform(value: TuiContainerResponse<Response>, _ui: &Ui) -> TuiContainerResponse<Response> {
+        value
+    }
+
+    fn field_error(
+        validation_errors: &Result<(), ValidationErrors>,
+        default_style: fn() -> Style,
+        tui: &mut Tui,
+        field_name: &str,
+    ) {
+        if let Err(errors) = validation_errors {
+            let errs = errors.field_errors();
+            if let Some(field_errors) = errs.get(field_name) {
+                tui.style(Style {
+                    grid_column: span(2),
+                    ..default_style()
+                })
+                    .add(|tui| {
+                        for field_error in field_errors.iter() {
+                            let code = &field_error.code;
+                            let params = &field_error.params;
+
+                            let args = build_fluent_args(params);
+
+                            let message = translate_fluent(code, &args);
+
+                            //trace!("field_error: {}", field_error);
+
+                            tui.label(RichText::new(message).color(colors::ERROR));
+                        }
+                    });
+            }
+        }
+    }
+}
+
+mod colors {
+    use egui::Color32;
+
+    pub const ERROR: Color32 = Color32::from_rgb(0xcb, 0x63, 0x5d);
+}
