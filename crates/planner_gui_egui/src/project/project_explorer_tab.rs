99719f2c53bb6d4c64ab5f1fb4ae5d42b1622d97
Dominic Clifton <me@dominicclifton.name>
2025-03-18 17:06:56 +0100

    PlannerGUI - Use egui_ltreeview instead of collapsible headers.
    * ltreeview supports multiple selection, drag-drop, etc.

--- crates/planner_gui_egui/src/project/project_explorer_tab.rs
+++ crates/planner_gui_egui/src/project/project_explorer_tab.rs
@@ -1,12 +1,13 @@
-use std::collections::HashMap;
-
+use derivative::Derivative;
 use egui::{Ui, WidgetText};
 use egui_i18n::{tr, translate_fluent};
-use egui_mobius::types::Enqueue;
+use egui_ltreeview::{TreeView, TreeViewBuilder, TreeViewState};
+use egui_mobius::types::{Enqueue, Value};
 use i18n::fluent_argument_helpers::planner_app::build_fluent_args;
 use petgraph::Graph;
 use petgraph::graph::NodeIndex;
 use planner_app::{ProjectTreeItem, ProjectTreeView};
+use tracing::trace;
 
 use crate::project::tabs::ProjectTabContext;
 use crate::project::{
@@ -14,10 +15,14 @@ use crate::project::{
 };
 use crate::tabs::{Tab, TabKey};
 
-#[derive(Debug)]
+#[derive(Derivative)]
+#[derivative(Debug)]
 pub struct ProjectExplorerUi {
     project_tree_view: Option<ProjectTreeView>,
-    project_tree_state: HashMap<NodeIndex, bool>,
+
+    // tuple struct containing old state and new state, state changes trigger events.
+    #[derivative(Debug = "ignore")]
+    tree_view_state: Value<(TreeViewState<usize>, TreeViewState<usize>)>,
     sender: Enqueue<(ProjectKey, ProjectUiCommand)>,
 }
 
@@ -25,52 +30,76 @@ impl ProjectExplorerUi {
     pub fn new(sender: Enqueue<(ProjectKey, ProjectUiCommand)>) -> Self {
         Self {
             project_tree_view: None,
-            project_tree_state: HashMap::new(),
+            tree_view_state: Default::default(),
             sender,
         }
     }
 
     fn show_project_tree(
         &self,
-        ui: &mut egui::Ui,
+        ui: &mut Ui,
         graph: &Graph<ProjectTreeItem, ()>,
         node: NodeIndex,
-        selection_state: &HashMap<NodeIndex, bool>,
+        project_key: &ProjectKey,
+    ) {
+        let mut tree_view_state = self.tree_view_state.lock().unwrap();
+
+        TreeView::new(ui.make_persistent_id("project_explorer_tree")).show_state(
+            ui,
+            &mut tree_view_state.0,
+            |builder: &mut egui_ltreeview::TreeViewBuilder<'_, usize>| {
+                self.show_project_tree_inner(builder, graph, node, project_key);
+            },
+        );
+
+        let selected = tree_view_state.0.selected();
+        let previously_selected = tree_view_state.1.selected();
+        if selected != previously_selected {
+            if selected.len() == 1 {
+                let node = *selected.first().unwrap();
+                let item = &graph[NodeIndex::new(node)];
+                let path = project_path_from_view_path(&item.path);
+
+                self.sender
+                    .send((*project_key, ProjectUiCommand::Navigate(path)))
+                    .expect("sent");
+            } else {
+                trace!("multi-select");
+            }
+        }
+
+        tree_view_state.1 = tree_view_state.0.clone();
+    }
+
+    fn show_project_tree_inner(
+        &self,
+        tree_builder: &mut TreeViewBuilder<usize>,
+        graph: &Graph<ProjectTreeItem, ()>,
+        node: NodeIndex,
         project_key: &ProjectKey,
     ) {
         let item = &graph[node];
-
-        let path = project_path_from_view_path(&item.path);
 
         let key = format!("project-explorer-node-{}", item.key);
         let args = build_fluent_args(&item.args);
 
         let label = translate_fluent(&key, &args);
 
-        let mut is_selected = if let Some(value) = selection_state.get(&node) {
-            *value
+        let node_id = node.index();
+
+        let mut node_created: bool = false;
+        for neighbour in graph.neighbors(node) {
+            if !node_created {
+                tree_builder.dir(node_id, &label);
+                node_created = true;
+            }
+            self.show_project_tree_inner(tree_builder, graph, neighbour, project_key);
+        }
+        if node_created {
+            tree_builder.close_dir();
         } else {
-            false
-        };
-
-        let id = ui.make_persistent_id(node);
-
-        egui::collapsing_header::CollapsingState::load_with_default_open(ui.ctx(), id, true)
-            .show_header(ui, |ui| {
-                if ui
-                    .toggle_value(&mut is_selected, label)
-                    .clicked()
-                {
-                    self.sender
-                        .send((*project_key, ProjectUiCommand::Navigate(path)))
-                        .expect("sent");
-                }
-            })
-            .body(|ui| {
-                for neighbor in graph.neighbors(node) {
-                    self.show_project_tree(ui, graph, neighbor, selection_state, project_key);
-                }
-            });
+            tree_builder.leaf(node_id, &label);
+        }
     }
 
     pub fn select_path(&mut self, project_path: &ProjectPath) {
@@ -87,9 +116,15 @@ impl ProjectExplorerUi {
             .node_indices()
             .find(|&index| graph[index].path.eq(&path))
         {
-            self.project_tree_state.clear();
-            self.project_tree_state
-                .insert(node, true);
+            let mut tree_view_state = self.tree_view_state.lock().unwrap();
+
+            let node_index = node.index();
+            tree_view_state
+                .0
+                .set_one_selected(node_index);
+            tree_view_state
+                .0
+                .expand_node(node_index);
         } else {
             unreachable!()
         }
@@ -116,13 +151,9 @@ impl Tab for ProjectExplorerTab {
     fn ui<'a>(&mut self, ui: &mut Ui, _tab_key: &TabKey, context: &mut Self::Context) {
         let state = context.state.lock().unwrap();
         if let Some(tree) = &state.project_tree.project_tree_view {
-            state.project_tree.show_project_tree(
-                ui,
-                &tree.tree,
-                NodeIndex::new(0),
-                &state.project_tree.project_tree_state,
-                &context.key,
-            );
+            state
+                .project_tree
+                .show_project_tree(ui, &tree.tree, NodeIndex::new(0), &context.key);
         } else {
             ui.centered_and_justified(|ui| {
                 ui.spinner();
