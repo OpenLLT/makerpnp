75be73823cf5e61d5d039f14903edd53f6307dc5
Dominic Clifton <me@dominicclifton.name>
2025-05-06 17:30:19 +0200

    PlannerGUI - Skeleton code to allow gerber files to be added/removed.

--- crates/planner_gui_egui/src/file_picker.rs
+++ crates/planner_gui_egui/src/file_picker.rs
@@ -16,7 +16,7 @@ enum PickerState {
     #[default]
     Pending,
     // use a boolean to indicate of picking has completed
-    Picking(Arc<Mutex<(bool, Option<PathBuf>)>>),
+    Picking(Arc<Mutex<(bool, Option<Vec<PathBuf>>)>>),
 }
 
 #[derive(Clone, PartialEq)]
@@ -31,7 +31,7 @@ impl Picker {
         matches!(self.state, PickerState::Picking(_))
     }
 
-    fn prepare(&mut self, some_fn: fn() -> Option<PathBuf>) {
+    fn prepare(&mut self, some_fn: fn() -> Option<Vec<PathBuf>>) {
         // initialise the boolean flag in the mutex to false, so that when the main thread continues it can see a
         // file has not been picked yet.  note that the mutex may not be locked until the picker thread starts to run
         // and lock it.
@@ -50,22 +50,28 @@ impl Picker {
         self.prepare(|| {
             rfd::FileDialog::new()
                 .pick_file()
-                .map(std::path::PathBuf::from)
+                .map(|file| vec![file])
         });
+    }
+
+    pub fn pick_files(&mut self) {
+        self.prepare(|| rfd::FileDialog::new().pick_files());
     }
 
     pub fn pick_folder(&mut self) {
         self.prepare(|| {
             rfd::FileDialog::new()
                 .pick_folder()
-                .map(std::path::PathBuf::from)
+                .map(|folder| vec![folder])
         });
     }
 
-    /// when picked, returns the picked path, or an error indicating the reason
+    /// when picked, returns the picked paths, or an error indicating the reason
+    ///
+    /// See also [`Self::picked`]
     ///
     /// this method is designed to be very fast while the picker is not picking (pending)
-    pub fn picked(&mut self) -> Result<PathBuf, PickError> {
+    pub fn picked_multi(&mut self) -> Result<Vec<PathBuf>, PickError> {
         let mut was_picked = false;
 
         let result = match &mut self.state {
@@ -100,5 +106,13 @@ impl Picker {
         }
 
         result
+    }
+
+    /// when picked, returns the first picked path, or an error indicating the reason
+    ///
+    /// use when picking a single file or folder, otherwise use [`Self::picked_multi`]
+    pub fn picked(&mut self) -> Result<PathBuf, PickError> {
+        self.picked_multi()
+            .map(|paths| paths.first().unwrap().clone())
     }
 }
---
+++ crates/planning/src/gerber.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+use pnp::pcb::PcbSide;
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, Clone)]
+pub struct GerberFile {
+    pub path: PathBuf,
+
+    pub purpose: GerberPurpose,
+    pub pcb_side: Option<PcbSide>,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum GerberPurpose {
+    Other,
+    Legend,
+    PasteMask,
+    SolderMask,
+    Assembly,
+    Copper,
+}
