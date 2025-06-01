d20cb76f23a94d989f5ab89ded84e423deb081f7
Dominic Clifton <me@dominicclifton.name>
2025-06-01 19:13:20 +0200

    WIP - PlannerAPP - Add support for fiducials.

--- crates/planner_gui_egui/src/ui_util.rs
+++ crates/planner_gui_egui/src/ui_util.rs
@@ -1,8 +1,8 @@
 use std::fmt::{Display, Formatter};
 use std::ops::Deref;
-use num_rational::Ratio;
 
 use egui::{Color32, Style};
+use num_rational::Ratio;
 
 pub fn green_orange_red_grey_from_style(style: &Style) -> (Color32, Color32, Color32, Color32) {
     let visual = &style.visuals;
@@ -59,7 +59,7 @@ pub fn ratio_of_f64(a: f64, b: f64) -> Option<Ratio<i64>> {
     }
     let ra = Ratio::approximate_float(a)?;
     let rb = Ratio::approximate_float(b)?;
-    
+
     // This automatically simplifies the result
     Some(ra / rb)
 }
@@ -67,6 +67,7 @@ pub fn ratio_of_f64(a: f64, b: f64) -> Option<Ratio<i64>> {
 #[cfg(test)]
 mod ratio_tests {
     use rstest::rstest;
+
     use super::ratio_of_f64;
 
     #[rstest]
@@ -75,12 +76,7 @@ mod ratio_tests {
     #[case(6.0, 9.0, 2, 3)]
     #[case(2.5, 5.0, 1, 2)]
     #[case(10.0, 4.0, 5, 2)]
-    fn test_ratio_of_f64(
-        #[case] a: f64,
-        #[case] b: f64,
-        #[case] expected_num: i64,
-        #[case] expected_denom: i64,
-    ) {
+    fn test_ratio_of_f64(#[case] a: f64, #[case] b: f64, #[case] expected_num: i64, #[case] expected_denom: i64) {
         let ratio = ratio_of_f64(a, b).expect("Failed to convert to ratio");
         assert_eq!(ratio.numer(), &expected_num);
         assert_eq!(ratio.denom(), &expected_denom);
