cc2aa07c3c398b91e37008c709b536a3382ee509
Dominic Clifton <me@dominicclifton.name>
2025-06-30 17:37:57 +0200

    PlannerCLI - Fix tests failing due to lack of `markdown` feature.
    * the previous attempt to stop the cargo warning was incorrect.

---
+++ crates/planner_cli/src/lib.rs
@@ -0,0 +1,2 @@
+// this file is currently here just to stop cargo from complaining when the `testing` dependency is used to run the
+// tests.  see `Cargo.toml` and the self-dependency in the  `[dev-dependencies]` section, and the `testing` feature.
