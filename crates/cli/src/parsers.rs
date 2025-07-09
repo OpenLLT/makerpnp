f490a3695bc9ce790ca8f5c66c16a5aca4e7742e
Dominic Clifton <me@dominicclifton.name>
2025-07-09 18:03:46 +0200

    PlannerCLI - Add dimension parsers.
    Clap doesn't help much here.
    
    * see this clap discussion started as a result of this commit: https://github.com/clap-rs/clap/discussions/6063

--- crates/cli/src/parsers.rs
+++ crates/cli/src/parsers.rs
@@ -1,10 +1,14 @@
+use std::collections::HashMap;
 use std::ffi::{OsStr, OsString};
 
 use clap::builder::TypedValueParser;
 use clap::error::ErrorKind;
 use clap::{value_parser, Arg, Command, Error};
+use nalgebra::Vector2;
 use planning::file::FileReference;
 use planning::placement::PlacementSortingItem;
+use pnp::panel::Dimensions;
+use rust_decimal::Decimal;
 
 use crate::args::{PlacementSortingModeArg, SortOrderArg};
 
@@ -63,5 +67,181 @@ impl TypedValueParser for FileReferenceParser {
             .ok_or_else(|| Error::raw(ErrorKind::InvalidValue, "Invalid argument encoding"))?;
 
         FileReference::try_from(value).map_err(|error| Error::raw(ErrorKind::InvalidValue, error.to_string()))
+    }
+}
+
+pub fn dimensions_decimal_parser(s: &str) -> Result<Dimensions<Decimal>, String> {
+    let mut values = HashMap::new();
+    let mut errors = Vec::new();
+    let required_keys = ["left", "right", "top", "bottom"];
+
+    for chunk in s.split(',') {
+        let chunk_chunks: Vec<_> = chunk.split('=').collect();
+        if chunk_chunks.len() != 2 {
+            errors.push(format!(
+                "Expected exactly 1 equal sign in '{}', found {}",
+                chunk,
+                chunk_chunks.len() - 1
+            ));
+            continue;
+        }
+
+        let key = chunk_chunks[0].trim();
+        let value_str = chunk_chunks[1].trim();
+
+        if !required_keys.contains(&key) {
+            errors.push(format!("Invalid key: '{}'", key));
+            continue;
+        }
+
+        match value_str.parse::<Decimal>() {
+            Ok(value) => {
+                values.insert(key, value);
+            }
+            Err(e) => {
+                errors.push(format!("Failed to parse decimal value for key '{}': {}", key, e));
+            }
+        }
+    }
+
+    // Make sure all fields are present
+    let mut missing = vec![];
+    for key in &required_keys {
+        if !values.contains_key(*key) {
+            missing.push(*key);
+        }
+    }
+
+    if !missing.is_empty() {
+        errors.push(format!(
+            "Missing/invalid keys: {}, expected keys: {}",
+            missing.join(", "),
+            required_keys.join(", ")
+        ));
+    }
+
+    if !errors.is_empty() {
+        return Err(errors.join("; "));
+    }
+
+    Ok(Dimensions {
+        left: values["left"],
+        right: values["right"],
+        top: values["top"],
+        bottom: values["bottom"],
+    })
+}
+#[cfg(test)]
+mod dimensions_parser_tests {
+    use rust_decimal_macros::dec;
+
+    use super::*;
+
+    #[test]
+    fn test_dimensions_parser() {
+        // when
+        let result = dimensions_decimal_parser("left=1,right=2.2,top=3.33,bottom=44.4");
+
+        // then
+        assert_eq!(
+            result,
+            Ok(Dimensions {
+                left: dec!(1.0),
+                right: dec!(2.2),
+                top: dec!(3.33),
+                bottom: dec!(44.4),
+            })
+        )
+    }
+
+    #[test]
+    fn test_dimensions_parser_errors() {
+        // when
+        let result = dimensions_decimal_parser("foo=bar,bar=foo,meh=blah=blah,right=XXX,left=1");
+
+        // then
+        assert_eq!(result, Err("Invalid key: 'foo'; Invalid key: 'bar'; Expected exactly 1 equal sign in 'meh=blah=blah', found 2; Failed to parse decimal value for key 'right': Invalid decimal: unknown character; Missing/invalid keys: right, top, bottom, expected keys: left, right, top, bottom".to_string()))
+    }
+}
+
+pub fn vector2_decimal_parser(s: &str) -> Result<Vector2<Decimal>, String> {
+    let mut values = HashMap::new();
+    let mut errors = Vec::new();
+    let required_keys = ["x", "y"];
+
+    for chunk in s.split(',') {
+        let chunk_chunks: Vec<_> = chunk.split('=').collect();
+        if chunk_chunks.len() != 2 {
+            errors.push(format!(
+                "Expected exactly 1 equal sign in '{}', found {}",
+                chunk,
+                chunk_chunks.len() - 1
+            ));
+            continue;
+        }
+
+        let key = chunk_chunks[0].trim();
+        let value_str = chunk_chunks[1].trim();
+
+        if !required_keys.contains(&key) {
+            errors.push(format!("Invalid key: '{}'", key));
+            continue;
+        }
+
+        match value_str.parse::<Decimal>() {
+            Ok(value) => {
+                values.insert(key, value);
+            }
+            Err(e) => {
+                errors.push(format!("Failed to parse decimal value for key '{}': {}", key, e));
+            }
+        }
+    }
+
+    // Make sure all fields are present
+    let mut missing = vec![];
+    for key in &required_keys {
+        if !values.contains_key(*key) {
+            missing.push(*key);
+        }
+    }
+
+    if !missing.is_empty() {
+        errors.push(format!(
+            "Missing/invalid keys: {}, expected keys: {}",
+            missing.join(", "),
+            required_keys.join(", ")
+        ));
+    }
+
+    if !errors.is_empty() {
+        return Err(errors.join("; "));
+    }
+
+    Ok(Vector2::new(values["x"], values["y"]))
+}
+
+#[cfg(test)]
+mod vector2_parser_tests {
+    use rust_decimal_macros::dec;
+
+    use super::*;
+
+    #[test]
+    fn test_vector2_parser() {
+        // when
+        let result = vector2_decimal_parser("x=1,y=22.222");
+
+        // then
+        assert_eq!(result, Ok(Vector2::new(dec!(1.0), dec!(22.222))))
+    }
+
+    #[test]
+    fn test_vector2_parser_errors() {
+        // when
+        let result = vector2_decimal_parser("foo=bar,bar=foo,meh=blah=blah,x=XXX,y=1");
+
+        // then
+        assert_eq!(result, Err("Invalid key: 'foo'; Invalid key: 'bar'; Expected exactly 1 equal sign in 'meh=blah=blah', found 2; Failed to parse decimal value for key 'x': Invalid decimal: unknown character; Missing/invalid keys: x, expected keys: x, y".to_string()))
     }
 }
