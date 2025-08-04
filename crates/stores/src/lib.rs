e7c6569c9c136c942b07649e4ecd97d165e079a3
Dominic Clifton <me@dominicclifton.name>
2025-08-04 11:34:41 +0200

    Stores - update module documentation.

--- crates/stores/src/lib.rs
+++ crates/stores/src/lib.rs
@@ -1,20 +1,19 @@
-pub mod eda_placements;
-pub mod part_mappings;
-/// Stores are for loading/storing different kinds of data.
-///
-/// Currently, all stores are just simple files, mostly CSV.
-///
-/// Example store backends:
-/// * Files (e.g. CSV).
-/// * Remote (e.g. REST).
-/// * Databases.
-/// * Etc.
-pub mod parts;
-pub mod placements;
-
+//! Stores are for loading/storing different kinds of data.
+//!
+//! Currently, all stores are just simple files, mostly CSV.
+//!
+//! Example store backends:
+//! * Files (e.g. CSV).
+//! * Remote (e.g. REST).
+//! * Databases.
+//! * Etc.
 pub mod assembly_rules;
 pub mod csv;
+pub mod eda_placements;
 pub mod load_out;
+pub mod part_mappings;
+pub mod parts;
+pub mod placements;
 pub mod substitutions;
 
 pub mod test;
